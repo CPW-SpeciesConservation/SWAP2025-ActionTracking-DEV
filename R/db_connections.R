065fa154d8cf322ccc729b35f4957427c917c19d
Adams <adamsc@dnr.state.co.us>
2026-04-02 10:12:36 -0600

    Update db_connections.R

--- R/db_connections.R
+++ R/db_connections.R
@@ -1,13 +1,19 @@
 library(DBI)
 library(RPostgres)
 
-readRenviron(".Renviron")
+# Safely load local variables, but ignore this step in the cloud!
+if (file.exists(".Renviron")) {
+  readRenviron(".Renviron")
+}
 
-dbConnect(
-  RPostgres::Postgres(),
-  dbname   = Sys.getenv("SUPABASE_DB"),
-  host     = Sys.getenv("SUPABASE_HOST"),
-  port     = as.integer(Sys.getenv("SUPABASE_PORT")),
-  user     = Sys.getenv("SUPABASE_USER"),
-  password = Sys.getenv("SUPABASE_PASSWORD")
-)
+# Wrap the connection in the function your app is looking for
+connect_supabase <- function() {
+  dbConnect(
+    RPostgres::Postgres(),
+    dbname   = Sys.getenv("SUPABASE_DB"),
+    host     = Sys.getenv("SUPABASE_HOST"),
+    port     = as.integer(Sys.getenv("SUPABASE_PORT")),
+    user     = Sys.getenv("SUPABASE_USER"),
+    password = Sys.getenv("SUPABASE_PASSWORD")
+  )
+}
